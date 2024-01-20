66a8d13d86f92719a6b3cb148f6d47743b303c69
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-20 05:34:17 +0000

    Rock paper sissors skeleton created

--- src/app.js
+++ src/app.js
@@ -7,5 +7,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // Your First Interactive Game
+  let playGame = confirm("Shall we play rock, paper, or scissors?");
+  if (playGame) {
+    // play
+    let playerChoice = prompt("Please enter rock, paper or scissors.");
+    if (playerChoice) {
+      let playerOne = playerChoice.trim().toLowerCase();
+      if (
+        playerOne === "rock" ||
+        playerOne === "paper" ||
+        playerOne === "scissors"
+      ) {
+        let computerChoice = Math.floor(Math.random() * 3 + 1);
+        let computer =
+          computerChoice === 1
+            ? "rock"
+            : computerChoice === 2
+            ? "paper"
+            : "scissors";
+        let result =
+          playerOne === computer
+            ? "Tie game!"
+            : playerOne === "rock" && computer === "paper"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\n Computer wins!`
+            : playerOne === "paper" && computer === "scissors"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : playerOne === "scissors" && computer === "rock"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+        alert(result);
+        let playAgain = confirm("Play again?");
+        playAgain ? location.reload() : alert("Ok, thanks for playing");
+      } else {
+        alert("you didnt enter rock, paper or scissors.");
+      }
+    } else {
+      alert("I guess you changed your mind. Mabye next time.");
+    }
+  } else {
+    alert("Ok, mabye next time.");
+  }
 };
